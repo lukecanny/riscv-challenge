eb33ca0ad9ff90b869c6fa87992e2c98a862aa6d
lukecanny <lcanny8@gmail.com>
2024-02-27 18:42:12 +0000

    project set up code added

--- riscv_driver.py
+++ riscv_driver.py
@@ -10,8 +10,26 @@ import struct
 
 class RISCV_App():
 
-    def __init__():
-        pass
+    def __init__(self, project_name="RV2Core"):
+        bitstream = project_name + ".bit"
+        self.rv = Overlay(bitstream)
+
+        # Define Input Signal (AXI GPIO)
+        self.rstN = self.rv.rstN
+        self.step = self.rv.step
+        self.runAll = self.rv.runAll
+        self.hostCtrl = self.rv.hostCtrl
+        self.host_memWr = self.rv.host_memWr
+        self.host_memAdd = self.rv.host_memAdd
+        self.host_datToMem = self.rv.host_datToMem
+        # Define Output Signal (AXI GPIO)
+        self.datToHost = self.rv.datToHost
+        # Define Internal Signal/Variables
+        self.period = 2e-08 #20ns
+        self.ABI_Array = ['zero','ra','sp','gp','tp','t0','t1','t2','s0/fp','s1','a0','a1','a2','a3','a4','a5','a6','a7',
+                    's2','s3','s4','s5','s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
+        self.WRITE_VIEW = 1; #0 = No printing. 1 = Print everything
+        self.DATA_VIEW = 0; #0 = Print Everything. 1 = Print only Data Locations
 
     def read_core_0_memory(self):
         pass
