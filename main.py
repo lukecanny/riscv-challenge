0d7876f6feda6cbdd7ce545ce2eb85de9ed44baa
lukecanny <lcanny8@gmail.com>
2024-02-27 18:31:55 +0000

    push stub-file

---
+++ main.py
@@ -0,0 +1,35 @@
+# Make a script that does the main processes of the jupyter notebook
+
+# Not all of these libraries are needed.
+from pynq import Overlay
+from time import sleep
+from pynq.overlays.base import BaseOverlay
+from pynq.lib import Pmod_ADC
+import matplotlib.pyplot as plt
+import struct
+
+class RISCV_App():
+
+    def __init__():
+        pass
+
+    def read_core_0_memory(self):
+        pass
+
+    def read_core_1_memory(self):
+        pass
+
+    def read_core_0_registers(self):
+        pass
+
+    def read_core_1_registers(self):
+        pass
+
+    def read_core_0_instruction_mem(self):
+        pass
+
+    def read_core_1_instruction_mem(self):
+        pass
+
+    def run_clock_cycles(self, number):
+        pass
